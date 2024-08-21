1636cae30e9f9116893a20af534a3bcada5e44ef
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-08-21 10:19:07 +0200

    add care train example

--- experiment_scripts/train_img.py
+++ experiment_scripts/train_img.py
@@ -9,7 +9,7 @@ from torch.utils.data import DataLoader
 import configargparse
 from functools import partial
 
-p = configargparse.ArgumentParser()
+p = configargparse.Arg  umentParser()
 p.add('-c', '--config_filepath', required=False, is_config_file=True, help='Path to config file.')
 
 p.add_argument('--logging_root', type=str, default='./logs', help='root for logging')
