241ae0ba8ecb9df3d8866b282fb7d7d76a3acad3
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-09-18 20:29:01 +0200

    train

--- modules.py
+++ modules.py
@@ -163,8 +163,8 @@ class FCBlock(MetaModule):
 class ImplicitMLP(nn.Module):
     def __init__(self):
         super(ImplicitMLP, self).__init__()
-        self.gff = GaussianFourierFeatureTransform(mapping_dim=64)
-        self.linear1 = nn.Linear(2 * 64, 128)
+        self.gff = GaussianFourierFeatureTransform(mapping_dim=84)
+        self.linear1 = nn.Linear(2 * 84, 128)
         self.linear2 = nn.Linear(128, 128)
         self.linear3 = nn.Linear(128, 64)
         self.linear4 = nn.Linear(64, 32)
