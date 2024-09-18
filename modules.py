9328443f4ee7517a6d116e3028cfbbf567b288a4
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-09-18 21:21:39 +0200

    train

--- modules.py
+++ modules.py
@@ -164,11 +164,11 @@ class ImplicitMLP(nn.Module):
     def __init__(self):
         super(ImplicitMLP, self).__init__()
         self.gff = GaussianFourierFeatureTransform(mapping_dim=84)
-        self.linear1 = nn.Linear(2 * 84, 168)
-        self.linear2 = nn.Linear(168, 128)
-        self.linear3 = nn.Linear(128, 32)
-        self.linear4 = nn.Linear(32, 16)
-        self.linear5 = nn.Linear(16, 3)
+        self.linear1 = nn.Linear(2 * 84, 128)
+        self.linear2 = nn.Linear(128, 128)
+        self.linear3 = nn.Linear(128, 64)
+        self.linear4 = nn.Linear(64, 32)
+        self.linear5 = nn.Linear(32, 3)
 
     def forward(self, model_input):
         h = 64
