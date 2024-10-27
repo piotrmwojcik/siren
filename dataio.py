373764c7aac984178c6bc15f2c69aec3a196b7ac
ABedychaj <9519957+ABedychaj@users.noreply.github.com>
2024-10-27 09:20:13 +0100

    srn cars and lmbdb loading

--- dataio.py
+++ dataio.py
@@ -4,6 +4,7 @@ import math
 import os
 import random
 
+import lmdb
 import matplotlib.colors as colors
 import numpy as np
 import scipy.io.wavfile as wavfile
@@ -57,6 +58,7 @@ def get_grid(h, w, b=0, norm=True, device="cpu"):
         return grid.permute(0, 3, 1, 2)  # [Batch, UV, H, W]
     else:
         return grid[0].permute(2, 0, 1)  # [UV, H, W]
+
 
 def lin2img(tensor, image_resolution=None):
     batch_size, num_samples, channels = tensor.shape
@@ -923,7 +925,7 @@ class ShapeNet(Dataset):
 
     def __len__(self):
         if self.split == "train":
-            return 20 # 35019
+            return 20  # 35019
         else:
             return 8762
 
@@ -1031,3 +1033,128 @@ class ShapeNetVoxel(Dataset):
         gt_dict = {'img': occs}
 
         return in_dict, gt_dict
+
+
+def format_for_lmdb(*args):
+    key_parts = []
+    for arg in args:
+        if isinstance(arg, int):
+            arg = str(arg).zfill(7)
+        key_parts.append(arg)
+    return '-'.join(key_parts).encode('utf-8')
+
+
+class SRNDatasetsLMDB(Dataset):
+    """
+    Dataset from SRN (V. Sitzmann et al. 2020)
+    PixelNeRF: https://github.com/sxyu/pixel-nerf/blob/master/src/data/SRNDataset.py
+    """
+
+    def __init__(
+            self, category, opt, split="train", world_scale=1.0, dataset_root='datasets', zero_to_one=False
+    ):
+        """
+        :param split train | val | test
+        :param image_size result image size (resizes if different)
+        :param world_scale amount to scale entire world by
+        """
+        if category != 'cars':
+            raise NotImplementedError("please category name of SRN Dataset")
+
+        self.base_path = os.path.join(dataset_root, 'srn_cars_lmdb', "cars_" + split)
+        assert os.path.exists(self.base_path)
+
+        self.env = lmdb.open(
+            self.base_path,
+            max_readers=32,
+            readonly=True,
+            lock=False,
+            readahead=False,
+            meminit=False,
+        )
+
+        if not self.env:
+            raise IOError('Cannot open lmdb dataset', self.base_path)
+
+        print("Loading SRN dataset", self.base_path, "name:")
+        with self.env.begin(write=False) as txn:
+            self.length = int(txn.get('length'.encode('utf-8')).decode('utf-8'))
+
+        self.split = split
+
+        self.image_to_tensor = Compose([
+            ToTensor(),
+            Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
+
+        self.debug = opt.debug
+        self.zero_to_one = True if opt.rgb_activation == 'sigmoid' or zero_to_one else False
+        self.subsampled_views = opt.subsampled_views
+        self.image_size = (opt.resolution, opt.resolution)
+        self.world_scale = world_scale
+        self._coord_trans = torch.diag(
+            torch.tensor([1, -1, -1, 1], dtype=torch.float32)
+        )
+
+        self.z_near = opt.near
+        self.z_far = opt.far
+        self.lindisp = False
+
+    def __len__(self):
+        if self.debug:
+            return 1  # for debug (NeRF check)
+        return self.length
+
+    def __getitem__(self, index):
+        all_imgs = []
+        all_poses = []
+
+        with self.env.begin(write=False) as txn:
+            intrins_byte = txn.get(format_for_lmdb(index, 'intrinsic'))
+            intrins = pickle.loads(intrins_byte)
+
+            focal, cx, cy = intrins[0][0], intrins[0][1], intrins[0][2]
+
+            l_key = format_for_lmdb(index, 'length')
+            length_txt = txn.get(l_key)
+            for frame_idx in range(0, int(length_txt)):
+                # i_key = f'{str(index).zfill(7)}img{str(frame_idx).zfill(7)}'.encode('utf-8')
+                # p_key = f'{str(index).zfill(7)}pose{str(frame_idx).zfill(7)}'.encode('utf-8')
+                i_key = format_for_lmdb(index, 'img', frame_idx)
+                p_key = format_for_lmdb(index, 'pose', frame_idx)
+
+                pose = pickle.loads(txn.get(p_key))
+
+                buffer = BytesIO(txn.get(i_key))
+                img = Image.open(buffer)
+
+                img_tensor = self.image_to_tensor(img)
+
+                all_imgs.append(img_tensor)
+
+                all_poses.append(pose)
+
+        all_imgs = torch.stack(all_imgs)
+        all_poses = torch.stack(all_poses)
+
+        if all_imgs.shape[-2:] != self.image_size:
+            scale = self.image_size[0] / all_imgs.shape[-2]
+            focal *= scale
+            cx *= scale
+            cy *= scale
+
+            all_imgs = F.interpolate(all_imgs, size=self.image_size, mode="area")
+
+        if self.world_scale != 1.0:
+            focal *= self.world_scale
+            all_poses[:, :3, 3] *= self.world_scale
+        focal = torch.tensor(focal, dtype=torch.float32)
+
+        if self.zero_to_one:
+            all_imgs = all_imgs * 0.5 + 0.5
+        in_dict = {'idx': index,
+                   'focal': focal,
+                   'c2w': all_poses}
+        out_dict = {'img': all_imgs}
+
+        return in_dict, out_dict
